76019073c418b39b1bca039637a2ee05190b9a4d
krishvaghasiya705 <kvaghasiya705@gmail.com>
2024-12-23 18:30:15 +0530

    created 90% of page of about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,14 @@
 import Aboutherobanner from "@/components/aboutpagecomponents/aboutherobanner";
+import Aboutmissionsection from "@/components/aboutpagecomponents/aboutmissionsection";
+import Autononewssection from "@/components/aboutpagecomponents/autononewssection";
 import React from "react";
 
 export default function About() {
   return (
     <>
       <Aboutherobanner />
+      <Aboutmissionsection/>
+      <Autononewssection/>
     </>
   );
 }
